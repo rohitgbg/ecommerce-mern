de6c36e19ac31812e9b7d9c34309b41f363b0bff
Rohith <rohitgbg@gmail.com>
2021-12-15 09:30:21 +0530

    lint fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,35 +2,51 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    jest: true,
+    jest: true
   },
   extends: [
-    "plugin:react/recommended",
-    "airbnb",
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:prettier/recommended",
+    'plugin:react/recommended',
+    'airbnb',
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended'
   ],
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
-      jsx: true,
+      jsx: true
     },
     ecmaVersion: 13,
-    sourceType: "module",
+    sourceType: 'module'
   },
-  plugins: ["react", "@typescript-eslint"],
+  plugins: ['react', '@typescript-eslint'],
   rules: {
-    "react/react-in-jsx-scope": "off",
-    "react/jsx-filename-extension": [1, { extensions: [".tsx", ".ts", ".js"] }],
-    "func-names": "off",
-    "no-undef": "off",
-    "react/function-component-definition": "off",
-    "@typescript-eslint/explicit-function-return-type": [
-      "error",
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-filename-extension': [1, { extensions: ['.tsx', '.ts', '.js'] }],
+    'func-names': 'off',
+    'no-undef': 'off',
+    'react/function-component-definition': 'off',
+    '@typescript-eslint/explicit-function-return-type': [
+      'error',
       {
-        allowExpressions: true,
-      },
+        allowExpressions: true
+      }
     ],
+    'import/prefer-default-export': 'off',
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': ['error'],
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        ts: 'never',
+        tsx: 'never'
+      }
+    ]
   },
+  settings: {
+    'import/resolver': {
+      typescript: {}
+    }
+  }
 };
